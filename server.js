9638357880089e83820adc9b87b93f7b8c844b00
Antonio de la Vega <antoniodelavegajimenez@gmail.com>
2016-10-13 00:24:48 +0200

    closes #5 - Rutas API usuarios

---
+++ server.js
@@ -0,0 +1,50 @@
+// BASE SETUP
+// ======================================
+
+var express    = require('express');
+var app        = express();
+var bodyParser = require('body-parser');
+var morgan     = require('morgan');
+var mongoose   = require('mongoose');
+var config 	   = require('./config');
+var path 	   = require('path');
+
+// APP CONFIGURATION ==================
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// configure our app to handle CORS requests
+app.use(function(req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+	next();
+});
+
+// log all requests to the console
+app.use(morgan('dev'));
+
+// connect to our database
+mongoose.connect(config.database);
+
+// set static files location
+// used for requests that our frontend will make
+app.use(express.static(__dirname + '/public'));
+
+// ROUTES FOR OUR API =================
+// ====================================
+
+// API ROUTES ------------------------
+var apiRoutes = require('./app/routes/api')(app, express);
+app.use('/api', apiRoutes);
+
+// MAIN CATCHALL ROUTE ---------------
+// SEND USERS TO FRONTEND ------------
+app.get('*', function(req, res) {
+	res.sendFile(path.join(__dirname + '/public/app/views/index.html'));
+});
+
+// START THE SERVER
+// ====================================
+app.listen(config.port);
+console.log('Port: ' + config.port);
