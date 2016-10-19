848052dce4acbc4f1f2869a0f0937455409f7098
Antonio de la Vega <antoniodelavegajimenez@gmail.com>
2016-10-20 00:02:58 +0200

    Configuración Socket.io

--- server.js
+++ server.js
@@ -7,7 +7,20 @@ var bodyParser = require('body-parser');
 var morgan     = require('morgan');
 var mongoose   = require('mongoose');
 var config 	   = require('./config');
-var path 	   = require('path');
+var path 	     = require('path');
+var async      = require('async');
+var request    = require('request');
+var xml2js     = require('xml2js');
+
+// Babel ES6/JSX Compiler
+require('babel-register');
+
+var swig  = require('swig');
+var React = require('react');
+var ReactDOM = require('react-dom/server');
+var Router = require('react-router');
+var routes = require('./app_front/routes');
+
 
 // APP CONFIGURATION ==================
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -38,12 +51,12 @@ app.use(express.static(__dirname + '/public'));
 // ====================================
 
 // API ROUTES ------------------------
-var apiRoutesAuth = require('./app/routes/api_auth')(app, express);
-var apiRoutesUser = require('./app/routes/api_user')(app, express);
-var apiRoutesMovement = require('./app/routes/api_movement')(app, express);
-var apiRoutesSet = require('./app/routes/api_set')(app, express);
-var apiRoutesExercise = require('./app/routes/api_exercise')(app, express);
-var apiRoutesSession = require('./app/routes/api_session')(app, express);
+var apiRoutesAuth = require('./app_back/routes/api_auth')(app, express);
+var apiRoutesUser = require('./app_back/routes/api_user')(app, express);
+var apiRoutesMovement = require('./app_back/routes/api_movement')(app, express);
+var apiRoutesSet = require('./app_back/routes/api_set')(app, express);
+var apiRoutesExercise = require('./app_back/routes/api_exercise')(app, express);
+var apiRoutesSession = require('./app_back/routes/api_session')(app, express);
 
 
 app.use('/api', apiRoutesAuth);
@@ -52,6 +65,22 @@ app.use('/api', apiRoutesMovement);
 app.use('/api', apiRoutesSet);
 app.use('/api', apiRoutesExercise);
 app.use('/api', apiRoutesSession);
+
+app.use(function(req, res) {
+  Router.match({ routes: routes.default, location: req.url }, function(err, redirectLocation, renderProps) {
+    if (err) {
+      res.status(500).send(err.message)
+    } else if (redirectLocation) {
+      res.status(302).redirect(redirectLocation.pathname + redirectLocation.search)
+    } else if (renderProps) {
+      var html = ReactDOM.renderToString(React.createElement(Router.RoutingContext, renderProps));
+      var page = swig.renderFile('views/index.html', { html: html });
+      res.status(200).send(page);
+    } else {
+      res.status(404).send('Page Not Found')
+    }
+  });
+});
 
 
 // MAIN CATCHALL ROUTE ---------------
@@ -62,5 +91,25 @@ app.get('*', function(req, res) {
 
 // START THE SERVER
 // ====================================
-app.listen(config.port);
-console.log('Port: ' + config.port);
+/**
+ * Socket.io stuff.
+ */
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var onlineUsers = 0;
+
+io.sockets.on('connection', function(socket) {
+  onlineUsers++;
+
+  io.sockets.emit('onlineUsers', { onlineUsers: onlineUsers });
+
+  socket.on('disconnect', function() {
+		console.log(onlineUsers);
+    onlineUsers--;
+    io.sockets.emit('onlineUsers', { onlineUsers: onlineUsers });
+  });
+});
+
+server.listen(8080, function() {
+  console.log('Express server listening on port ' + '8080');
+});
