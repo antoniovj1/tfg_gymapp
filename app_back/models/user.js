6ece59429b1855b2cfee5eb4b2c1ef390a56aa06
Antonio de la Vega <antoniodelavegajimenez@gmail.com>
2016-10-22 00:48:31 +0200

    Añadido rol a los usuarios

--- app_back/models/user.js
+++ app_back/models/user.js
@@ -9,27 +9,27 @@ var UserSchema   = new Schema({
   height: Number,
   weight: Number,
 	username: { type: String, required: true, index: { unique: true }},
-	password: { type: String, required: true, select: false }
+	password: { type: String, required: true, select: false },
+	role: {
+    type: String,
+    enum: ['Admin', 'Basic'],
+    default: 'Basic'
+  }
 });
 
-// hash the password before the user is saved
 UserSchema.pre('save', function(next) {
 	var user = this;
 
-	// hash the password only if the password has been changed or user is new
 	if (!user.isModified('password')) return next();
 
-	// generate the hash
 	bcrypt.hash(user.password, null, null, function(err, hash) {
 		if (err) return next(err);
 
-		// change the password to the hashed version
 		user.password = hash;
 		next();
 	});
 });
 
-// method to compare a given password with the database hash
 UserSchema.methods.comparePassword = function(password) {
 	var user = this;
 
