c7fc878637ec1385c208c9ff8aa6ee7cda21a1d0
Antonio de la Vega <antoniodelavegajimenez@gmail.com>
2016-11-02 13:07:24 +0100

    Componente Session Detail y componente Exercise

---
+++ public/js/components/Exercise.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from 'react-router'
+
+export default class Exercise extends React.Component {
+
+  render() {
+
+    const {group} = this.props;
+    const {exercise,movement,set} = group;
+
+    var i = 1;
+    const mappedSets = set.map(set =>
+    <div key= {set._id}>
+       <div class="col-lg-3"> {i++} </div>
+       <div class="col-lg-3"> {set.repetitions} </div>
+       <div class="col-lg-3"> {set.weight}  </div>
+       <div class="col-lg-3"> {set.rest}  </div>
+     </div>
+  )
+
+    return (
+    <div>
+      <div class="row well text-center" key = {movement._id}>
+      <h4> {movement.name} </h4>
+      <div class="col-lg-3"># </div>
+      <div class="col-lg-3">Reps </div>
+      <div class="col-lg-3">Peso </div>
+      <div class="col-lg-3">Rest </div>
+      {mappedSets}
+      </div>
+    </div>
+    );
+  }
+}
--- public/js/pages/SessionDetail.js
+++ public/js/pages/SessionDetail.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { connect } from "react-redux"
+import Exercise from "../components/Exercise"
+
+import {secondsToHms, dateFormat} from "../utils"
 
 import { fetchCompleteSession } from "../actions/sessionsActions"
 
@@ -12,17 +15,80 @@ import { fetchCompleteSession } from "../actions/sessionsActions"
 export default class SessionDetail extends React.Component {
 
   componentWillMount() {
-      var url = this.props.location.pathname
-      var id = url.substring(url.lastIndexOf("/")+1);
-      this.props.dispatch(fetchCompleteSession(id))
-    }
+    var url = this.props.location.pathname
+    var id = url.substring(url.lastIndexOf("/")+1);
+    this.props.dispatch(fetchCompleteSession(id))
+  }
+
 
   render() {
-  //  const {exercise} = this.props;
-  //  console.log(this.props);
-  const {completeSession} = this.props;
-    return (
-      <div> </div>
-    );
+
+    const {completeSession} = this.props;
+
+    if(completeSession) {
+      var {session,sets,movements,exercise} = completeSession;
+
+      var time = 0;
+      var date = "";
+      if(session){
+        if( session.time != 'undefinded')
+        time = secondsToHms(session.time);
+
+        if(typeof session.date != 'undefinded')
+        date = dateFormat(session.date);
+      }
+
+      var pesoTot = 0;
+      var nSets = 0;
+      if (sets) {
+        sets.forEach( function(subSet){
+          subSet.forEach(function(set) {
+            pesoTot+= set.weight;
+            nSets++;
+          });
+        });
+      }
+
+      var groups = [];
+
+      exercise.forEach(function (ex) {
+        var grouped = {exercise: ex, movement:null,set: null} ;
+        movements.forEach(function(mov){
+          if(mov._id == ex.movement){
+            grouped.movement = mov;
+          }
+        })
+        sets.forEach(function (set) {
+          if(set[0].exercise == ex._id){
+            grouped.set = set;
+          }
+        })
+        groups.push(grouped);
+      });
+    }
+
+    if(groups){
+      return (
+        <div>
+        <div class="row">
+        <div class = "col-md-3 well"> Fecha: {date} <br/><br/></div>
+        <div class = "col-md-3 well"> Tiempo: {time} <br/><br/></div>
+        <div class = "col-md-3 well"> Total series: {nSets}  <br/><br/></div>
+        <div class = "col-md-3 well"> Peso total: {pesoTot} Kg <br/><br/></div>
+        </div>
+        <div class="row">
+        <div class = "col-md-3 well"> TEST </div>
+        <div class = "col-md-9 well">
+        {groups.map(function(group) {
+          return <Exercise key={group.exercise._id} group={group}/>;
+        })}
+        </div>
+        </div>
+        </div>
+      );
+    } else {
+      return null;
+    }
+
   }
 }
