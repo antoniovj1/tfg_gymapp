b61dc00f114a0d6a302f04e92e3c1c01b6383857
Antonio de la Vega <antoniodelavegajimenez@gmail.com>
2016-11-07 03:19:26 +0100

    closes #27

--- app/routes/api_movement.js
+++ app/routes/api_movement.js
@@ -10,85 +10,86 @@ module.exports = function(app, express) {
   // /training/movements
   // -------------------
   apiRouter.route('/training/movements')
+
   // ===== POST =======
   .post(function(req, res) {
-
-  /*
-    {
+  /*{
     "name":"Dominadas",
     "material": "Barra",
     "muscles":[{"name":"bicep","percentage":20},
                {"name":"pecho","percentage":10},
                {"name":"dorsal","percentage":60},
                {"name":"abdominales","percentage":10}]
-      }
-  */
+      }*/
 
     var movement = new Movement(req.body);
 
-    movement.save(function(err, movement) {
-      if (err) {
-        if (err.code == 11000)
-          return res.json({ success: false, message: 'A movement with that name already exists. '});
-        else
-          return res.send(err);
-        }
-
-        res.json({ message: 'ok' ,movement });
-      });
-
+    movement.save()
+        .then(function(movement){
+            res.json({ message: 'ok' ,movement });
+        })
+        .catch(function(err){
+            if (err.code == 11000)
+                return res.json({ success: false, message: 'A movement with that name already exists. '});
+            else
+                return res.send(err);
+        })
   })
 
-  // ===== GET =======
-.get(function(req, res) {
+// ===== GET =======
+    .get(function(req, res) {
+        Movement.find({}).exec()
+            .then(function(movements){
+                res.json(movements);
+            })
+            .catch(function(err){
+                res.send(err);
+            })
+    });
 
-  Movement.find({}, function(err, movements) {
-    if (err) res.send(err);
-
-    res.json(movements);
-  });
-});
 
 // /training/movements/:name
 // -------------------
 apiRouter.route('/training/movements/:name')
 // ===== GET =======
-.get(function(req, res) {
-  Movement.find({name:req.params.name}, function(err, movement) {
-    if (err) res.send(err);
-
-    res.json(movement);
-  });
-})
+    .get(function(req, res) {
+        Movement.find({name:req.params.name}).exec()
+            .then(function(movement){
+                res.json(movement);
+            })
+            .catch(function(err){
+                res.send(err);
+            })
+    })
 
 // ===== PUT =======
-.put(function(req, res) {
-  Movement.findOne({name:req.params.name}, function(err, movement) {
+    .put(function(req, res) {
+        Movement.findOne({name:req.params.name}).exec()
+            .then(function(movement){
+                if (req.body.name) movement.name = req.body.name;
+                if (req.body.material) movement.material = req.body.material;
+                if (req.body.muscles) movement.muscles = req.body.muscles;
 
-    if (movement.length == 0)
-    return res.json({ success: false, message: 'fail'});
-    if (err)
-    return res.send(err);
-
-    if (req.body.name) movement.name = req.body.name;
-    if (req.body.material) movement.material = req.body.material;
-    if (req.body.muscles) movement.muscles = req.body.muscles;
-
-    movement.save(function(err, movement) {
-      if (err) res.send(err);
-      res.json({ message: 'ok', movement});
-    });
-
-  });
-})
+                return movement.save();
+            })
+            .then(function(movement){
+                res.json({ message: 'ok', movement});
+            })
+            .catch(function(err){
+                res.send(err);
+            })
+    })
 
 // ===== DELETE =======
-.delete(function(req, res) {
-  Movement.remove({	name:req.params.name }, function(err, user) {
-    if (err) res.send(err);
-    res.json({ message: 'ok' });
-  });
-});
+    .delete(function(req, res) {
+        Movement.remove({	name:req.params.name }).exec()
+            .then(function(){
+                res.json({ message: 'ok' });
+            })
+            .catch(function(err){
+                res.send(err);
+            })
+        });
 
 return apiRouter;
 };
