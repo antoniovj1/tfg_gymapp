10c4cb7a21b4dd53aaa860c2f2d0d4f2104fb169
Antonio de la Vega <antoniodelavegajimenez@gmail.com>
2016-11-16 11:14:40 +0100

    Actions

--- public/js/actions/loginActions.js
+++ public/js/actions/loginActions.js
@@ -2,30 +2,30 @@ import axios from "axios";
 var querystring = require('querystring');
 
 export function loginUser(user) {
-  return function(dispatch) {
-    dispatch({type: "LOGIN_REQUEST"});
+  return function (dispatch) {
+    dispatch({ type: "LOGIN_REQUEST" });
     axios.post("http://127.0.0.1:8080/api/authenticate",
-    querystring.stringify({
-      username: user.username,
-      password: user.password
-    }))
+      querystring.stringify({
+        username: user.username,
+        password: user.password
+      }))
       .then((response) => {
-        localStorage.setItem('id_token',response.data.token)
-        if(response.data.message == 'fail') {
-          dispatch({type: "LOGIN_FAILURE", payload: "Bad user or password"})
+        if (response.data.message == 'fail') {
+          dispatch({ type: "LOGIN_FAILURE", payload: "Bad user or password" });
         } else {
-          dispatch({type: "LOGIN_SUCCESS", payload: response.data})
+          localStorage.setItem('id_token', response.data.token);
+          dispatch({ type: "LOGIN_SUCCESS", payload: response.data });
         }
       })
       .catch((err) => {
-        dispatch({type: "LOGIN_FAILURE", payload: err})
+        dispatch({ type: "LOGIN_FAILURE", payload: err })
       })
   }
 }
 
 export function logoutUser(user) {
-  return function(dispatch) {
+  return function (dispatch) {
     localStorage.removeItem('id_token');
-    dispatch({type: "LOGOUT_SUCCESS"});
+    dispatch({ type: "LOGOUT_SUCCESS" });
   }
 }
--- public/js/actions/sessionsActions.js
+++ public/js/actions/sessionsActions.js
@@ -4,7 +4,7 @@ export function fetchSessions() {
   return function(dispatch) {
     var config = {headers: {'x-access-token': localStorage.getItem("id_token")}};
     dispatch({type: "FETCH_SESSION_PENDING"});
-    axios.get("http://127.0.0.1:8080/api/training/session", config)
+    axios.get("http://127.0.0.1:8080/api/training/sessions", config)
       .then((response) => {
         dispatch({type: "FETCH_SESSION_FULFILLED", payload: response.data})
       })
@@ -18,13 +18,29 @@ export function fetchCompleteSession(id) {
   return function(dispatch) {
     var config = {headers: {'x-access-token': localStorage.getItem("id_token")}};
     dispatch({type: "FETCH_COMPLETESESSION_PENDING"});
-    var url = "http://127.0.0.1:8080/api/training/session/byId/" + id;
+    var url = "http://127.0.0.1:8080/api/training/sessions/" + id;
     axios.get(url,config)
       .then((response) => {
         dispatch({type: "FETCH_COMPLETESESSION_FULFILLED", payload: response.data})
       })
       .catch((err) => {
         dispatch({type: "FETCH_COMPLETESESSION_REJECTED", payload: err})
+      })
+  }
+}
+
+export function pushSession(session) {
+  return function(dispatch) {
+    var config = {headers: {'x-access-token': localStorage.getItem("id_token")}};
+    var data = {session};
+
+    dispatch({type: "PUSH_SESSION_PENDING"});
+    axios.post("http://127.0.0.1:8080/api/training/sessions",data, config)
+      .then((response) => {
+        dispatch({type: "PUSH_SESSION_FULFILLED", payload: response.data})
+      })
+      .catch((err) => {
+        dispatch({type: "PUSH_SESSION_REJECTED", payload: err})
       })
   }
 }
