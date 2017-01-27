71f90c94a393090c5f34af73097ba1183a2c6137
Antonio de la Vega <antoniodelavegajimenez@gmail.com>
2017-01-27 01:49:00 +0100

    Más opciones

--- config.js
+++ config.js
@@ -2,6 +2,7 @@
 //-------
 module.exports = {
 'port': process.env.PORT || 8080,
+'database3' : 'mongodb://localhost:27017/iv',
 'database2': process.env.MONGODB_URI,
 'database': 'mongodb://' + process.env.MONGODB_PORT_27017_TCP_ADDR + ':' + process.env.MONGODB_PORT_27017_TCP_PORT + '/mydb',//process.env.MONGODB_URI,
 'secret': 'clave_secreta'
