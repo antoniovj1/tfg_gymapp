df361045d7eb439ba329f17ad42b3a226dd7fb8a
Antonio de la Vega <antoniodelavegajimenez@gmail.com>
2017-01-29 15:20:57 +0100

    No detener nginx ni mongo por si lo usa otra app.

--- fabfile.py
+++ fabfile.py
@@ -3,40 +3,48 @@ import os
 import time
 
 def info_servidor():
+    """Muestra informacion del servidor"""
     run ('cat /proc/cpuinfo')
 
 def install_app():
+    """Clona el repositorio e instala las dependencias"""
     run ('rm -rf infraestructura_virtual_ugr')
     run ('git clone https://github.com/antoniovj1/infraestructura_virtual_ugr.git')
     run ('cd infraestructura_virtual_ugr && npm install')
 
 def start_app():
+    """Inicia la aplicacion (node,mongo y nginx)"""
     run ('sudo service mongod start')
     run ('sleep 7 && cd infraestructura_virtual_ugr && sudo pm2 start server.js')
     run ('sudo service nginx restart')
 
 def restart_app():
+    """Reinicia la aplicacion"""
     run ('sudo service nginx restart')
-    run ('cd infraestructura_virtual_ugr && sudo pm2 restart server')
+    run ('sudo pm2 restart server')
 
 def logs_app():
-    run ('cd infraestructura_virtual_ugr && sudo pm2 logs server')
+    """Muestra los logs de la aplicacion"""
+    run ('sudo pm2 logs server')
     
 def stop_app():
-    run ('cd infraestructura_virtual_ugr && sudo pm2 stop server')
-    run ('sudo service mongod stop')
-    run ('sudo service nginx stop')
+    """Detiene la aplicación (node, mongo y nginx)"""
+    run ('sudo pm2 stop server')
+    # run ('sudo service mongod stop')
+    # run ('sudo service nginx stop')
 
 def kill_app():
-    run ('cd infraestructura_virtual_ugr && sudo pm2 delete server')
-    run ('sudo service mongod stop')
-    run ('sudo service nginx stop')
+    run ('sudo pm2 delete server')
+    # run ('sudo service mongod stop')
+    # run ('sudo service nginx stop')
 
 def update_app():
     installApp()
 
-def test_app():
-    run('sudo service mongod start')
-    run('cd infraestructura_virtual_ugr && sudo node server.js')
+def monitoring_pm2():
+    """Monitorización app"""
+    run ('sudo pm2 monit')
 
-    
+def list_pm2():
+    """Lista de apps ejecutandose"""
+    run ('sudo pm2 list')
