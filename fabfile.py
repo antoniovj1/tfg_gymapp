a9648d67a908abb14741ab78a8c8fc65e2ccc63e
Antonio de la Vega <antoniodelavegajimenez@gmail.com>
2017-01-29 13:15:44 +0100

    Nombres PEP 8

--- fabfile.py
+++ fabfile.py
@@ -5,37 +5,37 @@ import time
 def info_servidor():
     run ('cat /proc/cpuinfo')
 
-def installApp():
+def install_app():
     run ('rm -rf infraestructura_virtual_ugr')
     run ('git clone https://github.com/antoniovj1/infraestructura_virtual_ugr.git')
     run ('cd infraestructura_virtual_ugr && npm install')
 
-def startApp():
+def start_app():
     run ('sudo service nginx start')
     run ('sudo service mongod start')
     run ('sleep 7 && cd infraestructura_virtual_ugr && sudo pm2 start server.js')
 
-def restartApp():
+def restart_app():
     run ('sudo service nginx restart')
     run ('cd infraestructura_virtual_ugr && sudo pm2 restart server')
 
-def logsApp():
+def logs_app():
     run ('cd infraestructura_virtual_ugr && sudo pm2 logs server')
     
-def stopApp():
+def stop_app():
     run ('cd infraestructura_virtual_ugr && sudo pm2 stop server')
     run ('sudo service mongod stop')
     run ('sudo service nginx stop')
 
-def killApp():
+def kill_app():
     run ('cd infraestructura_virtual_ugr && sudo pm2 delete server')
     run ('sudo service mongod stop')
     run ('sudo service nginx stop')
 
-def updateApp():
+def update_app():
     installApp()
 
-def testApp():
+def test_app():
     run('sudo service mongod start')
     run('cd infraestructura_virtual_ugr && sudo node server.js')
 
