5fc273edfc763700a12fbe9752564617679388b0
Antonio de la Vega <antonio.v.j95@gmail.com>
2017-12-11 22:16:00 +0100

    Error Boundary Component

---
+++ public/js/components/ErrorBoundary.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+const style = {
+  width: '350px',
+  marginLeft: '20px',
+  marginTop: '20px',
+  display: 'inline-block'
+};
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false
+    };
+  }
+  componentDidCatch(error, info) {
+    this.setState({
+      hasError: true
+    });
+  }
+  render() {
+    if(this.state.hasError) {
+      return (
+        <h1> Algo fue mal... ERROR </h1>
+      );
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
