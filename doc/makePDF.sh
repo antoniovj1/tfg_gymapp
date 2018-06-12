8317cfd3a47bf649f81855f25bf27bfa832f7280
Antonio de la Vega <antonio.v.j95@gmail.com>
2018-06-12 04:58:06 +0200

    Ajustes #105

--- doc/makePDF.sh
+++ doc/makePDF.sh
@@ -2,14 +2,21 @@
 
 #Generación de PDF compilando el archivo LaTeX con pdflatex
 pdflatex -synctex=1 -interaction=nonstopmode  proyecto.tex > /dev/null 2>&1
+echo "Generados archivos auxiliares"
+makeglossaries proyecto > /dev/null 2>&1
+echo "Generado glosario"
+pdflatex -synctex=1 -interaction=nonstopmode  proyecto.tex > /dev/null 2>&1
+pdflatex -synctex=1 -interaction=nonstopmode  proyecto.tex > /dev/null 2>&1
 echo "PDF -> proyecto.pdf"
 
 rm *.aux
 rm *.lof
 rm *.log
-rm *.lol
 rm *.lot
 rm *.out
+rm *.toc
+rm *.ist
+rm *.gls
+rm *.glo
+rm *.glg
 rm *.synctex.gz
-rm *toc
-
